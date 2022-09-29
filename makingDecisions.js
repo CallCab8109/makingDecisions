06d334d533b4134bafb9e860c85499405343395b
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-09-28 20:17:57 -0600

    problem 6 solved

--- makingDecisions.js
+++ makingDecisions.js
@@ -94,7 +94,8 @@ console.log('hello')
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
 
-//CODE HERE
+for(let i=0; i<= 9; i++)
+console.log(i + 1)
 
 ////////// PROBLEM 7 //////////
 
